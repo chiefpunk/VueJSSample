0f396a7d4aa3da3bb9af1918558d8e594e3d6bdd
Maksim Damaskin <risingstar1002@mail.ru>
2019-08-20 10:26:06 +1000

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,51 @@
-const path = require('path');
 
-// check for env file
-let env;
-try {
-	// (base)/
-	// require ('./env-stg');
-	// using exports, as when require from .vue, build causes warning 'Module not found: Error: Can't resolve './env-stg''
-	env = require ('./env');
-	env = env.env;
-} catch (e) {
-	// default prod
-	env = 'prod';
-}
-
-// check for env based nuxt config when called from different relative paths
-let configPath;
-let config;
-try {
-	// (base)/
-	configPath = `./nuxt.config-${env}.js`;
-	// config = require(configPath); // on build, results in warning 'Critical dependency: the request of a dependency is an expression'
-	config = require(`./nuxt.config-${env}.js`);
-} catch (e) {
-	try {
-		// (base)/server/
-		configPath = `../nuxt.config-${env}.js`;
-		config = require(`../nuxt.config-${env}.js`);
-	} catch (e) {
-		// (base)/pages/dir/
-		configPath = `../../nuxt.config-${env}.js`;
-		config = require(`../../nuxt.config-${env}.js`);
-	}
-}
-
-// TODO remove logging; visual validation of correct env
-console.log('Building nuxt using ' + configPath);
-module.exports = config;
+module.exports = {
+	mode: 'spa',
+	router: { middleware: 'auth' },
+	head: {
+		title: 'IconGPS Campaign Manager',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Material+Icons|Roboto:300,400,700&display=swap' },
+			{ rel: 'stylesheet', href: '/css/vue-datetime.min.css' }
+		],
+		script: [
+			{ src: "https://code.jquery.com/jquery-3.3.1.slim.min.js", integrity: "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo", crossorigin: "anonymous" },
+			{ src: "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js", integrity: "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1", crossorigin: "anonymous" },
+			{ src: "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js", integrity: "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM", crossorigin: "anonymous" },
+			{ src: "/js/scripts.js" }
+		]
+	},
+	css: [ '@/assets/scss/app.scss' ],
+	loading: { color: '#7BE52F' },
+	modules: [
+		['bootstrap-vue/nuxt', { css: false }],
+		'@nuxtjs/axios',
+		'@nuxtjs/auth'
+	],
+	auth: {
+		strategies: {
+			local: {
+				endpoints: {
+					login: { url: 'login', method: 'post', propertyName: 'jwt' },
+					user: { url: 'me', method: 'get', propertyName: false },
+					logout: false
+				},
+				tokenRequired: true,
+				tokenType: null,
+				tokenName: 'token',
+			}
+		},
+		redirect: {
+			forgot: '/forgot'
+		}
+	},
+	plugins: [ '~/plugins/utils', '~/plugins/vue-js-modal', '~/plugins/vue-notification', '~/plugins/vue-pagination' ],
+	axios: { baseURL: 'http://{ikongps-cm-web}' }, // no trailing /
+	server: { port: 9003 },
+	validator: { maxBodyLength : 255 }
+};
